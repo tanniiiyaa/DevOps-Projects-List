a77b643865a53396a97948ae20779ac52eb5b2d8
Tan <ntaniyaa7@gmail.com>
2026-06-29 13:24:10 +0530

    Rename output variables for ALB module

--- DevOps-Project-01/infrastructure/modules/alb/variables.tf
+++ DevOps-Project-01/infrastructure/modules/alb/variables.tf
@@ -15,17 +15,17 @@ variable "public_subnets" {
   type        = list(string)
 }
 
-output "alb_arn_output" {
+output "alb_arn" {
   description = "ALB ARN"
   value       = aws_lb.main.arn
 }
 
-output "alb_dns_name_output" {
+output "alb_dns_name" {
   description = "ALB DNS name"
   value       = aws_lb.main.dns_name
 }
 
-output "target_group_arn_output" {
+output "target_group_arn" {
   description = "Target group ARN"
   value       = aws_lb_target_group.main.arn
 }
