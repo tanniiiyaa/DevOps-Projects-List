efb92601c63a0099405e1af7e8a96827b6a0f5a0
Tan <ntaniyaa7@gmail.com>
2026-06-29 12:29:58 +0530

    Rename RDS output variables for consistency

--- DevOps-Project-01/infrastructure/modules/rds/outputs.tf
+++ DevOps-Project-01/infrastructure/modules/rds/outputs.tf
@@ -1,14 +1,14 @@
-output "rds_instance_id" {
+output "rds_instance_id_output" {
   description = "RDS instance ID"
   value       = aws_db_instance.main.id
 }
 
-output "rds_instance_endpoint" {
+output "rds_instance_endpoint_output" {
   description = "RDS instance endpoint"
   value       = aws_db_instance.main.endpoint
 }
 
-output "rds_instance_port" {
+output "rds_instance_port_output" {
   description = "RDS instance port"
   value       = aws_db_instance.main.port
 }
