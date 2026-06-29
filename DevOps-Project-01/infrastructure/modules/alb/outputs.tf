c834a8b49a66ba976d04bf11ecd7f38df0ef7225
Tan <ntaniyaa7@gmail.com>
2026-06-29 12:27:53 +0530

    Rename output variables in ALB module

--- DevOps-Project-01/infrastructure/modules/alb/outputs.tf
+++ DevOps-Project-01/infrastructure/modules/alb/outputs.tf
@@ -3,12 +3,12 @@ output "alb_arn_output" {
   value       = aws_lb.main.arn
 }
 
-output "alb_dns_name" {
+output "alb_dns_name_output" {
   description = "ALB DNS name"
   value       = aws_lb.main.dns_name
 }
 
-output "target_group_arn" {
+output "target_group_arn_output" {
   description = "Target group ARN"
   value       = aws_lb_target_group.main.arn
 }
